28090aeb1d246b2f1e1a43d961ce668cda9016fa
Thauanny <thauannyk@ufrn.edu.br>
2021-06-21 13:39:18 -0300

    pacote ajustado

--- trabalho01/src/trabalho/questao05/Questao05.java
+++ trabalho01/src/trabalho/questao05/Questao05.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trabalho.questao05;
+package trabalho01.src.trabalho.questao05;
 
-import trabalho.Tree;
+import trabalho01.src.trabalho.Tree;
 
 public class Questao05{
 	public static void main(String[] args) { 
