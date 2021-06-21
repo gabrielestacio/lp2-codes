3b5db67bb2759fdca674823f299879e76e564699
Thauanny <thauannyk@ufrn.edu.br>
2021-06-21 13:34:31 -0300

    questao 05

---
+++ trabalho01/src/trabalho/questao05/Questao05.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho.questao05;
+
+import trabalho.Tree;
+
+/**
+ *
+ * @author Thauanny
+ */
+
+
+public class Questao05{
+	public static void main(String[] args) { 
+        Tree tree =  new Tree();
+    
+        tree.inOrder();
+    }
+            
+              
+}
