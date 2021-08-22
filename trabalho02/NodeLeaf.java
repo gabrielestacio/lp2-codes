b4d2c52b9247006d5dc919f09c4f323426b17b19
Thauanny <thauannyk@ufrn.edu.br>
2021-08-22 19:59:19 -0300

    questoes 1, 2, 3 e 4

--- trabalho02/NodeLeaf.java
+++ trabalho02/NodeLeaf.java
@@ -1,3 +1,12 @@
+/*
+	TRABALHO 02 - LINGUAGEM DE PROGRAMAÇÃO 2 - 2021.1
+	GABRIEL ESTACIO E THAUANNY RAMOS
+	_________________________________________________
+	
+	OBSERVAÇÕES DO ARQUIVO:
+		CLASSE QUE DEFINE O TIPO NODELEAF, UTILIZADO NA QUESTÃO 6.
+ */
+
 package trabalho02;
 
 public class NodeLeaf implements ExpressionNode{
